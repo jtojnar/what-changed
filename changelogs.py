1825700304daaeb8f4f821b82465d19c5aebd856
Jan Tojnar <jtojnar@gmail.com>
2019-09-14 20:11:35 +0200

    Port to cache.json
    Closes: https://github.com/jtojnar/what-changed/issues/1

--- changelogs.py
+++ changelogs.py
@@ -1,36 +1,12 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -p python3 -p python3.pkgs.libversion -p python3.pkgs.beautifulsoup4 -p python3.pkgs.requests -i python3
+#! nix-shell -p python3 -p python3.pkgs.libversion -p python3.pkgs.requests -i python3
 
-from bs4 import BeautifulSoup
-from enum import Enum
-from itertools import chain, groupby
 from libversion import Version
-from typing import Generator, Iterator, NamedTuple, Tuple
+from typing import Dict, Iterator, List, Tuple
 import argparse
-import re
+import json
 import requests
-
-class Kind(Enum):
-    '''Types of items in a directory listing.'''
-    PARENT = 0
-    DIR = 1
-    FILE = 2
-
-def kind_from_src(src: str) -> Kind:
-    '''Convert icon path to a type of an item. Unfortunately, there is no good way to obtain file type from Apache directory listing so we are relying on item icons to discriminate the files.'''
-    mappings = {
-        '/icons/back.png': Kind.PARENT,
-        '/icons/folder.png': Kind.DIR,
-        '/icons/unknown.png': Kind.FILE,
-        '/icons/compressed.png': Kind.FILE,
-    }
-
-    return mappings.get(src, Kind.FILE)
-
-class Item(NamedTuple):
-    name: str
-    url: str
-    kind: Kind
+import sys
 
 def version_branch(version: str) -> str:
     '''Extract branch (major plus minor segments) out of a version string.'''
@@ -52,90 +28,48 @@ def fetch_text(uri: str) -> str:
     '''Fetch text from a given URI.'''
     return requests.get(uri).text
 
-def list_dir(url: str) -> Generator[Item, None, None]:
-    '''Parse Apache directory listing from GNOME web servers.'''
-    page = fetch_text(url)
-    soup = BeautifulSoup(page, 'html.parser')
+VersionFiles = Dict[str, str]
+VersionsFiles = Dict[str, VersionFiles]
+Versions = List[str]
+BranchFiles = Dict[str, List[str]]
 
-    for row in soup.find_all('tr'):
-        img = row.select_one('td img')
-
-        if img:
-            kind = kind_from_src(img.get('src'))
-
-            if kind != Kind.PARENT:
-                link = row.find('a')
-
-                yield Item(
-                    name=link.get('href'),
-                    url=url + link.get('href'),
-                    kind=kind,
-                )
-
-def get_branch_dirs(pname: str) -> Iterator[Item]:
-    url = f'https://download.gnome.org/sources/{pname}/'
-
-    items = list_dir(url)
-
-    return filter(lambda i: i.kind == Kind.DIR, items)
-
-def name_to_base(name: str) -> str:
-    '''Find a base part of a file name (i.e. ${project}-${version}).'''
-    suffix_pattern = re.compile(r'\.(tar\.(xz|bz2|gz)|changes|news|sha256sum)$')
-
-    if not suffix_pattern.search(name):
-        raise RuntimeError(f'Unexpected file name “{name}”')
-
-    return suffix_pattern.sub('', name)
-
-def name_to_version(pname: str, name: str) -> Version:
-    '''Extract just a version from a file name.'''
-    return Version(name_to_base(name).lstrip(f'{pname}-'))
-
-def get_change(group: Tuple[str, Iterator[Item]]) -> str:
+def get_change(pname: str, version_name: str, group: VersionFiles) -> str:
     '''From a group of files for certain version, try to obtain files with changes. Either hand-written news or changes generated from the list of commits.'''
-    name, files = group
 
-    changes = None
-
-    for file in files:
-        if file.name.endswith('.changes'):
-            changes = file.url
-        elif file.name.endswith('.news'):
-            changes = file.url
-            break
+    changes = group.get('news', group.get('changes'))
 
     if changes:
-        # There are no hand-written news, only a list of commits
-        return fetch_text(changes)
+        return fetch_text(f'https://ftp.gnome.org/pub/GNOME/sources/{pname}/{changes}')
 
-    return f'No file describing changes found for group in cluster with “{name}”'
+    return f'No file describing changes found for group in cluster with “{version_name}”'
 
-def get_branch_changes(branch: Item, pname: str, start_version: Version, end_version: Version):
-    all_files = list_dir(branch.url)
-    branch_name = branch.name.rstrip('/')
+def get_version_changes(pname: str, version: Version, version_files: VersionsFiles) -> str:
+    return get_change(pname, version.value, version_files[version.value])
 
-    version_files = filter(lambda i: i.kind == Kind.FILE and i.name.startswith(f'{pname}-{branch_name}.') and start_version <= name_to_version(pname, i.name) <= end_version, all_files)
+def fetch_cache(pname: str) -> Tuple[VersionsFiles, Versions, BranchFiles]:
+    '''Obtain a cache.json file for a given package and return the parsed content.'''
+    cache = json.loads(fetch_text(f'https://ftp.gnome.org/pub/GNOME/sources/{pname}/cache.json'))
 
-    # the files already appear to be sorted
-    version_file_groups = groupby(version_files, lambda i: name_to_base(i.name))
+    # The structure of cache.json: https://gitlab.gnome.org/Infrastructure/sysadmin-bin/blob/master/ftpadmin#L762
+    if not isinstance(cache, list) or cache[0] != 4:
+        print('Unknown format of cache.json file.', file=sys.stderr)
+        sys.exit(1)
 
-    return map(get_change, version_file_groups)
+    _jsonversion, version_files, versions, branch_files = cache
 
-def get_changes(pname: str, start_version: Version, end_version: Version) -> Iterator[Item]:
-    start_branch = Version(version_branch(start_version.value))
-    end_branch = Version(version_branch(end_version.value))
+    return (version_files[pname], versions[pname], branch_files)
 
-    def is_relevant(branch):
-        version = Version(branch.name.rstrip('/'))
+def get_changes(pname: str, start_version: Version, end_version: Version) -> Iterator[str]:
+    version_files, versions, _branch_files = fetch_cache(pname)
 
-        return start_branch <= version <= end_branch
+    def is_relevant(version):
+        return start_version <= version <= end_version
 
-    relevant_branches = filter(is_relevant, get_branch_dirs(pname))
+    relevant_versions = sorted(filter(is_relevant, map(Version, versions)))
 
-    changes = map(lambda branch: get_branch_changes(branch, pname, start_version, end_version), relevant_branches)
+    version_changes = map(lambda version: get_version_changes(pname, version, version_files), relevant_versions)
 
-    return chain(*changes)
+    return version_changes
 
 def main():
     parser = argparse.ArgumentParser(description='Obtain the list of changes between two versions of GNOME.')
