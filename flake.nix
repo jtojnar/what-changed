0c5271ca1dae2a6d6bfec78f0e626ba300bc88d3
Jan Tojnar <jtojnar@gmail.com>
2021-10-04 16:20:03 +0200

    nix: pin flake-compat using flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
   description = "Tool for obtaining changelogs for Nixpkgs";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
 
-  inputs.utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system: let
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs { inherit system; };
   in {
     devShell = pkgs.mkShell {
