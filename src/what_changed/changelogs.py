13c692f4e1941c07833bfbec038f0adf872f63fd
Jan Tojnar <jtojnar@gmail.com>
2021-10-05 05:40:33 +0200

    Move utils to utils

--- src/what_changed/changelogs.py
+++ src/what_changed/changelogs.py
@@ -1,39 +1,10 @@
 from .formatters import Formatter
+from .utils import fetch_text
 from libversion import Version
 from typing import Dict, Iterator, List, Optional, Tuple
 import json
-import requests
 import re
 import sys
-
-def version_branch(version: str) -> str:
-    '''Extract branch (major plus minor segments) out of a version string.'''
-    return '.'.join(version.split('.')[0:2])
-
-def version_major(version: str) -> str:
-    '''Extract major segment out of a version string.'''
-    return version.split('.')[0]
-
-def version_minor(version: str) -> str:
-    '''Extract minor segment out of a version string.'''
-    return version.split('.')[1]
-
-def version_patch(version: str) -> str:
-    '''Extract patch segment out of a version string.'''
-    return version.split('.')[2]
-
-def fetch_text(uri: str) -> Optional[str]:
-    '''Fetch text from a given URI.'''
-    response = requests.get(uri)
-
-    # GNOME files are likely UTF-8 so let’s not fallback to ISO
-    if response.encoding is None:
-        response.encoding = 'utf-8'
-
-    if response.ok:
-        return response.text
-
-    return None
 
 VersionFiles = Dict[str, str]
 VersionsFiles = Dict[str, VersionFiles]
